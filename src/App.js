7d319d2b7f9ee5d3e3a1959e4ff93c0a9a4eaebd
Adamari14 <adamarim_15@hotmail.com>
2022-08-24 17:32:42 -0500

    Añadiendo algo de estilos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import ListOfGifs from './components/ListOfGifs'; 
 
 export default function App() {
-  const[keyword, setKeyword] = useState('panda')
-
   return (
     <div className="App">
       <section className="App-content">
-        <button onClick={() => setKeyword('mapache')}>Cambiar keyword</button>
-      <ListOfGifs keyword={keyword} />
+      <ListOfGifs keyword="panda" />
       </section>
     </div>
   )
