3863a8be25e17143b05f8bc0a2ac51d0638a9e07
Adamari14 <adamarim_15@hotmail.com>
2022-08-24 16:49:49 -0500

    Creando componente ListOfGifs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import React, {useEffect ,useState} from 'react';
+import React from 'react';
 import './App.css';
-import getGifs from './services/getGifs';
-import Gif from './components/Gif'; 
-
+import ListOfGifs from './components/ListOfGifs'; 
 
 export default function App() {
-  const [gifs, setGifs] = useState([])
-  
-  useEffect(function(){
-    getGifs({keyword: 'programing'}).then(gifs => setGifs(gifs))
-  }, [])
-
   return (
     <div className="App">
       <section className="App-content">
-      {
-        gifs.map(({id, title, url}) => 
-          <Gif
-            key={id}
-            title={title} 
-            url={url} 
-            id={id}
-          />
-        )
-        
-      }
+      <ListOfGifs keyword='Ecuador' />
       </section>
     </div>
-  );
+  )
 }
