4e887007f3f5fd8ec7b8ce5791e0cc065130bbef
Adamari14 <adamarim_15@hotmail.com>
2022-08-29 16:53:42 -0500

    Añadiendo un form que haga de buscador de Gifs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link,  Route } from "wouter";
 import './App.css';
-import ListOfGifs from './components/ListOfGifs'; 
 
 export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <h1>App</h1>
-        <Link to='/gif/panda'>Gifs de pandas</Link>
-        <Link to='/gif/ecuador'>Gifs de Ecuador</Link>
-        <Link to='/gif/chile'>Gifs de Chile</Link>
+        <Link to="/">
+          <img className='App-logo' alt='Giffy logo' src='/logo.png' />
+        </Link>
         <Route 
-          path='/gif/:keyword' 
-          component={ListOfGifs }
+          component={Home}
+          path= "/"
         />
+        <Route 
+          component={SearchResults}
+          path= "/search/keyword" />
+        <Route 
+        component={Detail}
+        path= "/gif/:id"
+      />
       </section>
     </div>
   )
--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,10 +2,12 @@ import React from "react";
 import './Gif.css'
 
 export default function Gif({title, id, url}){
-    return (
-        <a href={`#${id}`} className = 'Gif'>
-          <h4>{title}</h4>
-          <img alt={title} src = {url}/>
-        </a>
-    )
+  return (
+    <div className="Gif">
+      <Link to={`/gif/${id}`} className = 'Gif-link'>
+        <h4>{title}</h4>
+        <img alt={title} src = {url}/>
+      </Link>
+    </div>
+  )
 }
