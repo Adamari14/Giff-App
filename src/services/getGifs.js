04e82e2fbcf7b373a6226b08f463d5c0904b1264
Adamari14 <adamarim_15@hotmail.com>
2022-08-29 17:39:42 -0500

    useGifs, haciendo que use el localStorage

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -3,17 +3,17 @@ const apiKey = '5LoxDPdQ2ixQkQ0NCghrFzLGfY5KeNFq'
 export default function getGifs ({keyword = 'morty'} = {}) {
   const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
 
-  return fetch(apiURL)
-    .then(res => res.json())
-    .then(response => {
-      const {data = []} = response
-      if(Array.isArray(data)) {
-          const gifs = data.map(image => {
-              const {images, title , id} = image 
-                const {url} = image.images.downsized_medium.url
-                return{title, id,url}
-          })
-          return gifs
-      }
-    })
-}
+  return  fetch(apiURL)
+  .then(res => res.json())//de la respuesta convertirla como ojson
+  .then(response => {
+    const {data/*es un array */=[]} = response
+    if(Array.isArray(data)) {  
+      const gifs = data.map(image => {  
+        const {images, title, id}=image
+        const {url}=images.downsized_medium
+        return {title, id, url} 
+      })
+      return gifs
+    }
+  })
+} 
