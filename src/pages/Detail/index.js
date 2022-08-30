8c25a1359166e34d460d034033c3ae19233591ac
Adamari14 <adamarim_15@hotmail.com>
2022-08-30 17:29:05 -0500

    custom Hooks

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,9 @@
 import React, {useContext} from 'react'
-import GifsContext from '../../context/GifsContext'
 import Gif from '../../components/Gif'
+import useGlobalGifs from '../../hooks/useGlobalGifs'
 
 export default function Detail({params}){
-    const {gifs} = useContext(GifsContext)
+    const gifs = useGlobalContext()
 
     const gif = gifs.find(singleGif => 
         singleGif.id == params.id
