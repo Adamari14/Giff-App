6b6b52a50c591e58ce20ee987daac489868bd10a
adamari14 <adamarim_15@hotmail.com>
2022-08-31 17:27:57 -0500

    Reestructuración

---
+++ src/components/TrendingSearches/index.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import getTrendingTerms from 'services/getTrendingTermsService'
+import Category from 'Category'
+
+function TrendingSearches (){
+    const [trends, setTrends] = useState([])
+
+    useEffect(function () {
+        getTrendingTerms().then(setTrends) 
+    },[] )
+
+    return <Category name='Tendencias' options={trends} />
+}
+
+export default function LazyTrending () {
+    const[show, setShow] = useState(false)
+
+    useEffect(function () {
+        const onChange = (entries) => {
+            const el = entries[0]
+            if (el.isIntersecting){
+                setShow(true)
+            }
+        }
+ 
+        const observer = new IntersectionObserver(onChange, {
+            rootMargin: '100px'
+        }) 
+
+        observer.observer(document.getElementById('LazyTrending'))
+    })
+
+    return <div id='LazyTrending'>
+        {show ? <TrendingSearches/> : null}
+    </div>
+} 
---
+++ src/services/settings.js
@@ -0,0 +1,3 @@
+export const API_KEY = '5LoxDPdQ2ixQkQ0NCghrFzLGfY5KeNFq'
+
+export const API_URL = 'https://api.giphy.com/v1'
