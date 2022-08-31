3c2cca0e64daa9b2dedbb2c2788fec60088e93df
adamari14 <adamarim_15@hotmail.com>
2022-08-31 16:55:22 -0500

    Añadir Trendings de Giphy

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,19 +1,22 @@
-const apiKey = '5LoxDPdQ2ixQkQ0NCghrFzLGfY5KeNFq'
+import {API_KEY, API_URL} from './settings'
 
-export default function getGifs ({keyword = 'morty'} = {}) {
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+const fromApiResponseToGifs = apiResponse => {
+  const {data = []} = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const {images, title, id} = image
+      const { url } = images.downsized_medium
+      return { title, id, url }
+    })
+    return gifs
+  }
+  return []
+}
 
-  return  fetch(apiURL)
-  .then(res => res.json())//de la respuesta convertirla como ojson
-  .then(response => {
-    const {data/*es un array */=[]} = response
-    if(Array.isArray(data)) {  
-      const gifs = data.map(image => {  
-        const {images, title, id}=image
-        const {url}=images.downsized_medium
-        return {title, id, url} 
-      })
-      return gifs
-    }
-  })
-} 
+export default function getGifs ({limit = 25, keyword = 'morty', page = 0} = {}) {
+  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=G&lang=en`
+
+  return fetch(apiURL)
+    .then(res => res.json())
+    .then(fromApiResponseToGifs)
+}
