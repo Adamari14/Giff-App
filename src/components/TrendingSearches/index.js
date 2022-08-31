899ffd19c9284c2bde2801ba04af3a6b46d17700
adamari14 <adamarim_15@hotmail.com>
2022-08-31 18:31:01 -0500

    useRef para guardar elementos del DOM

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react'
 import getTrendingTerms from 'services/getTrendingTermsService'
-import Category from 'Category'
+import Category from 'components/Category'
 
 function TrendingSearches (){
     const [trends, setTrends] = useState([])
@@ -17,10 +17,11 @@ export default function LazyTrending () {
     const elementRef = useRef()
 
     useEffect(function () {
-        const onChange = (entries) => {
+        const onChange = (entries, observer) => {
             const el = entries[0]
             if (el.isIntersecting){
                 setShow(true)
+                observer.disconnect()
             }
         }
  
@@ -29,6 +30,8 @@ export default function LazyTrending () {
         }) 
 
         observer.observer(elementRef.current)//valor de la referencia
+
+        return () => observer.disconnect()
     })
 
     return <div ref={elementRef}>
