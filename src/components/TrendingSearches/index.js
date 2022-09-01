7ff004c9d26024256a76fa87e22b38a6e79149b2
adamari14 <adamarim_15@hotmail.com>
2022-08-31 20:02:59 -0500

    Cargar un componente de forma dinámica con React.lazy
    Carga js solo cuando se necesite

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,22 +1,12 @@
-import React, {useState, useEffect} from 'react'
-import getTrendingTerms from 'services/getTrendingTermsService'
-import Category from 'components/Category'
+import React from 'react'
 import useNearScreen from 'hooks/useNearScreen'
 
-function TrendingSearches (){
-    const [trends, setTrends] = useState([])
-
-    useEffect(function () {
-        getTrendingTerms().then(setTrends) 
-    },[] )
-
-    return <Category name='Tendencias' options={trends} />
-}
+const TredingSearches = React.lazy(
+    () => import('./TrendingSearches') //import dinamico, solo carga cuando se necesite
+)
 
 export default function LazyTrending () {
     const {isNearScreen, fromRef} = useNearScreen({distance: '200px'})
-
-    
 
     return <div ref={fromRef}>
         {isNearScreen ? <TrendingSearches/> : null}
