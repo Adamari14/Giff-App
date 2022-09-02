56cc9d1a9c012eecdc3965814cda0339ec1294ce
adamari14 <adamarim_15@hotmail.com>
2022-09-02 16:15:40 -0500

    Infinite Scroll

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Spinner from 'components/Spinner'
 import ListOfGifs from 'components/ListOfGifs'
 import {useGifs} from 'hooks/useGifs'
+import useNearScreen from 'hooks/useNearScreen'
 
 export default function SearchResults ({ params }) {
   const { keyword } = params
   const { loading, gifs, setPage } = useGifs({ keyword })
+  const {isNearScreen, fromRef} = useNearScreen()
 
-  const handleNextPage = () => setPage(prevPage => prevPage + 1)
+  //const handleNextPage = () => setPage(prevPage => prevPage + 1)
 
   return <>
     {loading
@@ -17,6 +19,7 @@ export default function SearchResults ({ params }) {
           {decodeURI(keyword)}
         </h3>
         <ListOfGifs gifs={gifs} />
+        <div id="visor" ref={fromRef}></div>
       </>
     }
     <br />
