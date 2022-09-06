497fd95d89d8fc07c67981633f9ad3baf5748ac1
adamari14 <adamarim_15@hotmail.com>
2022-09-06 17:27:51 -0500

    Testeando eventos y formulario

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement } from '@testing-library/react';
+import { fireEvent, render, waitForElement , screen} from '@testing-library/react';
 import App from '../App';
 
 test('home work as expecte', async() => {
@@ -10,3 +10,15 @@ test('home work as expecte', async() => {
    
   expect(gifLink).toBeVisible()
 });
+
+test('serch form could be used', async () => {
+    render(<App />)
+    const  input = await screen.findByRole('textbox')
+    const button = await screen.findByRole('button')
+
+    fireEvent.change(input, {target: {value: 'Matrix'}})
+    fireEvent.click(button)
+
+    const title = await screen.findByText('Matrix')
+    expect(title).toBeVisible()
+})
