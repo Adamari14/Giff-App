24deee749174658c39044b34ad6bfd69f9bcf594
adamari14 <adamarim_15@hotmail.com>
2022-09-07 17:23:59 -0500

    Extraer componente SearchForm

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,25 @@
-import React, {useState} from "react"
+import React from "react"
 import { useLocation } from "wouter"
 import ListOfGifs from 'components/ListOfGifs'
 import {useGifs} from 'hooks/useGifs'
 import TrendingSearches from "components/TrendingSearches"
+import SearchForm from "components/SearchForm"
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('')
   const [path, pushLocation] = useLocation()
   const {loading, gifs} = useGifs()
 
-  const handleSubmit = evt => {
-    evt.preventDefault()
+  const handleSubmit = ({keyword}) => {
     // navegar a otra ruta
     pushLocation(`/search/${keyword}`)
   }
 
-  const handleChange = evt => {
-    setKeyword(evt.target.value)
-  }
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
-      </form>
+      <SearchForm onSubmit={handleSubmit} />
       <div className="App-main">
         <div className="App-results">
-          <h3 className="App-title">Última</h3>
+          <h3 className="App-title">Última búsqueda</h3>
           <ListOfGifs gifs={gifs} />
         </div>
         <div className="App-category">
