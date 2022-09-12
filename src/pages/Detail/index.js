3a427f9d271a6dc4235071c7298722b4589c7fb1
adamari14 <adamarim_15@hotmail.com>
2022-09-12 17:37:08 -0500

    Añadiendo isLoading e isError al hook useSingleGif

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -3,8 +3,9 @@ import Gif from 'components/Gif'
 import useSingleGifs from 'hooks/useSingleGif'
 
 export default function Detail ({ params }) {
-  const {gif} = useSingleGifs({id: params.id})
+  const {gif, isLoading, isError} = useSingleGifs({id: params.id})
 
+  if (isLoading) return <Spinner />
   if(!gif) return null
 
   return <>
